26235f8bb1addc1df08543a151fb1c1e6d419d57
Oliver Fei <ofei006@uottawa.ca>
2022-12-27 19:52:10 -0500

    Revert "test different kat note shape"
    This reverts commit 3e7bac43feb88359788e71b24c76029c5af23953.

--- src/graphics/three/taiko/DonKatNotesManager.ts
+++ src/graphics/three/taiko/DonKatNotesManager.ts
@@ -4,7 +4,6 @@ import {
   Vector4,
   RingBufferGeometry,
   BufferGeometryUtils,
-  PlaneBufferGeometry,
 } from "three";
 import { createClampedVisibiltyMaterial } from "../../../objects/note";
 import InstancedMeshObjectPool from "../InstancedMeshObjectPool";
@@ -15,34 +14,20 @@ const COLOR_BLUE = new Vector4(0, 0.6, 0.8, 1);
 
 const BASE_POSITION = new Vector3(0, 0.0001, 100);
 
-const dot = new CircleBufferGeometry(0.01, 24);
-const line = new PlaneBufferGeometry(0.3, 0.02, 1, 1);
-
-const smallDonRadius = 0.08;
-const SMALLDON = BufferGeometryUtils.mergeBufferGeometries([
-  new CircleBufferGeometry(smallDonRadius, 24),
-]);
+const SMALLDON = new CircleBufferGeometry(0.08, 24);
 SMALLDON.rotateX(-Math.PI / 2);
-
-const largeDonRadius = 0.13;
-const LARGEDON = BufferGeometryUtils.mergeBufferGeometries([
-  new CircleBufferGeometry(largeDonRadius, 24),
-]);
+const LARGEDON = new CircleBufferGeometry(0.13, 24);
 LARGEDON.rotateX(-Math.PI / 2);
 
-const katAngle = Math.PI / 8;
-const startAngle = 0 - katAngle / 2;
-const endAngle = startAngle + Math.PI;
+const dot = new CircleBufferGeometry(0.02, 16);
 const SMALLKAT = BufferGeometryUtils.mergeBufferGeometries([
-  new RingBufferGeometry(0.15, 0.21, 8, 1, startAngle, katAngle),
-  new RingBufferGeometry(0.15, 0.21, 8, 1, endAngle, katAngle),
-  line,
+  new RingBufferGeometry(0.15, 0.21, 24, 1, 0, Math.PI),
+  dot,
 ]);
 SMALLKAT.rotateX(-Math.PI / 2);
 const LARGEKAT = BufferGeometryUtils.mergeBufferGeometries([
-  new RingBufferGeometry(0.15, 0.3, 16, 1, startAngle, katAngle),
-  new RingBufferGeometry(0.15, 0.3, 16, 1, endAngle, katAngle),
-  line,
+  new RingBufferGeometry(0.15, 0.3, 48, 1, 0, Math.PI),
+  dot,
 ]);
 LARGEKAT.rotateX(-Math.PI / 2);
 
