2d8c4ce5c6affe2d1d73be07e5dada3d95eb5b47
Oliver Fei <ofei006@uottawa.ca>
2023-01-02 21:47:11 -0500

    Prevent stars from spawning around note rail area

--- src/components/starfield.js
+++ src/components/starfield.js
@@ -1,12 +1,22 @@
 AFRAME.registerComponent("starfield", {
   init: function () {
     const vertices = [];
+    const invalidRegion = new THREE.Box3(
+      new THREE.Vector3(-0.6, 0, 0.2),
+      new THREE.Vector3(0.6, 2.5, -8)
+    );
+    const point = new THREE.Vector3();
     for (let i = 0; i < 1000; i++) {
-      const x = THREE.MathUtils.randFloatSpread(50);
-      const y = THREE.MathUtils.randFloat(0, 25);
-      const z = THREE.MathUtils.randFloatSpread(50);
-      vertices.push(x, y, z);
-      vertices.push(x, -y, z);
+      point.x = THREE.MathUtils.randFloatSpread(50);
+      point.y = THREE.MathUtils.randFloat(0, 25);
+      point.z = THREE.MathUtils.randFloatSpread(50);
+      while (invalidRegion.containsPoint(point)) {
+        point.x = THREE.MathUtils.randFloatSpread(50);
+        point.y = THREE.MathUtils.randFloat(0, 25);
+        point.z = THREE.MathUtils.randFloatSpread(50);
+      }
+      vertices.push(point.x, point.y, point.z);
+      vertices.push(point.x, -point.y, point.z);
     }
     const starsGeometry = new THREE.BufferGeometry();
     starsGeometry.setAttribute(
